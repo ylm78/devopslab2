c9fe10f88ff85c3253f9892b57d899fa6d5b02dc
yilmaz-a <arnaud.yilmaz@efrei.net>
2025-05-16 15:34:11 +0200

    initial

--- simple-api/src/main/java/fr/takima/training/simpleapi/SimpleApiApplication.java
+++ simple-api/src/main/java/fr/takima/training/simpleapi/SimpleApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleapiApplication.class, args);
+		SpringApplication.run(SimpleApiApplication.class, args);
 	}
 
 }
