f3f800eebb44b48191349b09ff32d1cebda44d90
yilmaz-a <arnaud.yilmaz@efrei.net>
2025-05-16 16:25:42 +0200

    z

--- simple-api/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
+++ simple-api/src/test/java/fr/takima/training/sampleapplication/IT/DepartmentControllerTestIT.java
@@ -17,7 +17,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest(classes = { SimpleApiApplication.class })
 class DepartmentControllerTestIT {
-
     @Autowired
     private MockMvc mockMvc;
 
--- simple-api/src/test/java/fr/takima/training/sampleapplication/IT/StudentControllerTestIT.java
+++ simple-api/src/test/java/fr/takima/training/sampleapplication/IT/StudentControllerTestIT.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
-@SpringBootTest(classes = { SimpleapiApplication.class })
+@SpringBootTest(classes = { SimpleApiApplication.class })
 class StudentControllerTestIT {
 
     @Autowired
